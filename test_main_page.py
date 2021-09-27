c49b8f3dc1d261a1343c98f2c79221ad794ecac7
lostdevil95 <48276079+lostdevil95@users.noreply.github.com>
2021-09-27 17:44:57 +0300

    tests was replaced for improving test logic. and test login page added in test suite

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,23 @@
 from .pages.main_page import MainPage
 from time import sleep
+from .pages.login_page import LoginPage
+
+LINK = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, LINK)
+    page.open()
+    page.should_be_login_link()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = MainPage(browser, LINK)
     page.open()
     page.go_to_login_page()
     sleep(5)
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
+def test_should_be_login_page(browser):
+    page = LoginPage(browser, LINK)
+    page.open()
+    page.should_be_login_page()
