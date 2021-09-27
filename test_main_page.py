ed13bb6cad4226c1c12178447a3c60098fed5b0b
lostdevil95 <48276079+lostdevil95@users.noreply.github.com>
2021-09-27 19:08:01 +0300

    init LoginPage in test body

--- test_main_page.py
+++ test_main_page.py
@@ -14,10 +14,4 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, LINK)
     page.open()
     page.go_to_login_page()
-    sleep(5)
-
-
-def test_should_be_login_page(browser):
-    page = LoginPage(browser, LINK)
-    page.open()
-    page.should_be_login_page()
+    LoginPage(browser, browser.current_url).should_be_login_page()
