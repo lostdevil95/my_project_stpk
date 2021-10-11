2bd5398716ce673622d2cea46ff246b8d3b17e18
lostdevil95 <48276079+lostdevil95@users.noreply.github.com>
2021-10-11 12:14:51 +0300

    some

--- pages/locators.py
+++ pages/locators.py
@@ -10,3 +10,7 @@ class LoginPageLocators:
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
     LOGIN_URL = (By.CSS_SELECTOR, '#login_link')
 
+class ProductPageLocators:
+    ADD_TO_CART = (By.CSS_SELECTOR, "button[value='Добавить в корзину']")
+    PRICE_COLOR = (By.CSS_SELECTOR, "div[class='col-sm-6 product_main'] p[class='price_color']")
+    PRICE_CART = ''
